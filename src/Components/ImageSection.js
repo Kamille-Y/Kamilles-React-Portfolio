51181b3e0c1baf1dfd10575214d3b90f4aaf4d62
Kamille <tramia25@gmail.com>
2021-08-13 11:55:04 -0400

    semi complete portfolio

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import about from '../img/about.jpg';
 
-function ImageSection() {
+function HomePage() {
     return (
-        <div className="ImageSection">
+        <div className="HomePage">
             <div className="img">
                 <img src={about} alt=""/>
             </div>
             <div className="about-info">
-                <h4>I am<span> Lorem Ipsum</span></h4>
+                <h4>I am<span> Kamille Young</span></h4>
                 <p className="about-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Voluptatum necessitatibus perferendis numquam dolorem cumque facilis.
-                    Desir per po la ta doso vi slone ve?
+                   I am new full stack Developer. I really enjoy UX/UI and styling. 
+                   I hope to hone my database and middleware skills over time. I desire to 
+                   obtain work as a freelance while I expand and build my ideas.
                 </p>
                 <div className="about-details">
                     <div className="left-section">
@@ -24,12 +24,12 @@ function ImageSection() {
                         <p>Countries</p>
                     </div>
                     <div className="right-section">
-                        <p>: Lorem Ipsum</p>
-                        <p>: 50</p>
-                        <p>: French</p>
-                        <p>: French, Spanish, English</p>
-                        <p>: 2 Beightbridge, BN7 GB9, United kingdom</p>
-                        <p>: France, Spain, Germany</p>
+                        <p>: Kamille Tramia Young</p>
+                        <p>: 35</p>
+                        <p>: English</p>
+                        <p>: English, Some Spanish</p>
+                        <p>: Cleveland Ohio</p>
+                        <p>: United States</p>
                     </div>
                 </div>
                 <button className="btn">Download Cv</button>
@@ -38,4 +38,4 @@ function ImageSection() {
     )
 }
 
-export default ImageSection;
+export default HomePage;
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -11,22 +11,20 @@ function HomePage() {
             <header className="hero">
                 <h1 className="hero-text">
                     Hi, I am 
-                    <span> Lorem Ipsum.</span>
+                    <span> Kamille T. Young.</span>
                 </h1>
                 <p className="h-sub-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Cupiditate facere, numquam vitae placeat consequatur corrupti officia 
-                    quibusdam. Blanditiis doloremque nemo ex facilis neque. 
-                    A sint ipsam earum nemo omnis et.
+                    I am a new Full Stack Developer. I am really good with styling. I am working to become
+                   build database and middleware skills. 
                 </p>
                 <div className="icons">
-                    <Link to="test" className="icon-holder">
+                    <Link to="https://www.facebook.com/She.2.Silly/" className="icon-holder">
                         <FontAwesomeIcon icon={faFacebook} className="icon fb" />
                     </Link>
-                    <Link to="test" className="icon-holder">
+                    <Link to="https://github.com/Kamille-Y" className="icon-holder">
                         <FontAwesomeIcon icon={faGithub} className="icon gh" />
                     </Link>
-                    <Link to="test" className="icon-holder">
+                    <Link to="https://www.youtube.com/channel/UCSeCZKxQeIv_b-qzoc0PESw" className="icon-holder">
                         <FontAwesomeIcon icon={faYoutube} className="icon yt"/>
                     </Link>
                 </div>
